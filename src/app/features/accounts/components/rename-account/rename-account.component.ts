b489bf47692013a65d5e5bf9c5dbfee6f62c2c83
Blake Arceneaux <b.arceneaux@meridian.internal>
2020-12-03 09:16:00 -0500

    MOL-79 accounts: rename account, transaction detail

---
+++ src/app/features/accounts/components/rename-account/rename-account.component.ts
@@ -0,0 +1,41 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+import { AppError } from '../../../../core/errors/app-error.model';
+import { AccountsApiService } from '../../../../core/api/accounts-api.service';
+
+export interface RenameAccountData {
+  accountId: string;
+  nickname: string;
+}
+
+/** Nickname editor dialog. */
+@Component({
+  selector: 'mol-rename-account',
+  templateUrl: './rename-account.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RenameAccountComponent {
+  busy = false;
+  error: AppError | null = null;
+
+  constructor(
+    readonly ref: MatDialogRef<RenameAccountComponent, boolean>,
+    @Inject(MAT_DIALOG_DATA) readonly data: RenameAccountData,
+    private readonly api: AccountsApiService,
+    private readonly cdr: ChangeDetectorRef
+  ) {}
+
+  confirm(): void {
+    this.busy = true;
+    this.error = null;
+    this.api.rename(this.data.accountId, this.data.nickname).subscribe({
+      next: () => this.ref.close(true),
+      error: (err: AppError) => {
+        this.busy = false;
+        this.error = err;
+        this.cdr.markForCheck();
+      }
+    });
+  }
+}
